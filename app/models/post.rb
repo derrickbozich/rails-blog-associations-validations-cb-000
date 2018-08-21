54982b6eae833dc8d0bd572bb1644f54a8bee90b
derrick b <dbozich1@gmail.com>
2018-08-21 22:02:51 +0000

    automatically backed up by learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,5 @@
 class Post < ActiveRecord::Base
   belongs_to :user
-  has_many :posts_tags
-  has_many :tags, through: :posts_tags
+  has_many :post_tags
+  has_many :tags, through: :post_tags
 end
